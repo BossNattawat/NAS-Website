256689792bd9f4299bac82294beb6e6ada550faa
Nattawat <BossNattawat@users.noreply.github.com>
2024-12-09 20:58:46 +0700

    Update nav() function

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function updateMenuDisplay() {
-    const menu = document.querySelector('.menu');
-    if (window.innerWidth > 950) {
-        menu.style.display = 'flex';
-    } else {
-        menu.style.display = 'none';
-    }
+  const menu = document.querySelector('.menu');
+  if (window.innerWidth > 950) {
+      menu.style.display = 'flex';
+  } else {
+      menu.style.display = 'none';
+  }
 }
 
 updateMenuDisplay();
 
 window.addEventListener('resize', updateMenuDisplay);
 
-function myFunction() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
+function nav() {
+  var x = document.getElementById("menu");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
 }
